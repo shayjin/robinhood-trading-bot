6945b3bc37ed19368ebca52b3e1940e320180500
Jay Shin <shin.810@osu.edu>
2023-06-02 15:19:53 -0700

    개같은놈들들 라이브러리 고장내놨네

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ EMAIL = data["EMAIL"]
 PWD = data["PASSWORD"]
 CODE = data["CODE"]
 DISCORD_WEBHOOK_URL = data["DISCORD_WEBHOOK_URL"]
-BUY_AMOUNT = 0.5
+BUY_AMOUNT = 1
 
-login = robinhood.login(EMAIL, PWD, mfa_code=CODE)
 topt = pyotp.TOTP(KEY).now()
+login = robinhood.login(EMAIL, PWD, mfa_code=CODE)
 
-companies = ["PLTR", "DELL", "GOOGL", "AMZN", "EXPE"]
+companies = ["PLTR", "DELL", "AMZN", "EXPE"]
 company_list = {}
 done = []
 company_target = {}
@@ -32,12 +32,14 @@ def send_message(msg):
     requests.post(DISCORD_WEBHOOK_URL, data=message)
 
 def buy(company, amount):
-    robinhood.order_buy_market(company, amount)
-    send_message("[Bought " + str(amount) + " " + company + " stocks]")
+    #robinhood.order_buy_market(company, amount)
+    #send_message("[Bought " + str(amount) + " " + company + " stocks]")
+    send_message("[Buy " + company + " right now!!!]")
 
-def sell(company, amount):
-    robinhood.order_sell_market(company, amount)
-    send_message("[Sold " + str(amount) + " " + company + " stocks]")
+def sell(company, amount, bought, sold):
+    #robinhood.order_sell_market(company, amount)
+    #send_message("[Sold " + str(amount) + " " + company + " stocks]")
+    send_message("[Sell " + company + " for " + str(float(sold/bought)) + "% right now!!!]")
     done.append(company)
 
 for company in companies:
@@ -54,6 +56,7 @@ for company in companies:
     target_price = start_price + (yesterday_high - yesterday_low) * 0.5
     company_target[company] = target_price
 
+
 while True:
     current_hour = now.hour
     current_minute = now.minute
@@ -64,12 +67,12 @@ while True:
             target_price = company_target[company]
 
             if current_price >= target_price and bought_price == 0:
-               # buy(company, BUY_AMOUNT)
+                buy(company, BUY_AMOUNT)
                 company_list[company] = current_price
             
-            if bought_price > 0 and (current_price - bought_price) >= bought_price * 0.3:
+            if bought_price > 0 and (current_price - bought_price) >= bought_price * 0.015:
                 sold_amount = robinhood.build_holdings()[company]['quantity']
-                #sell(company, sold_amount)
+                sell(company, sold_amount, company_list[company], current_price)
         
         for d in done:
             if d in company_list:
